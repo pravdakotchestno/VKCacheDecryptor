6a6c3ad35998e4e457cebb70838bca6cd5e3494c
pravdakotchestno <pravdakotchestno@protonmail.ch>
2019-04-18 16:02:57 +0700

    Update decryptor.py

--- decryptor.py
+++ decryptor.py
@@ -1,18 +1,33 @@
-def keygen(l):
-	key = b'\x0D\x1E\x2F\x40\x51\x62\x73\x84\x95\xA6\xB7\xC8\xD9\xEA\xFB\x0C'
-	for i in range(15):
-		for j in range(16):
-			key += bytes([(key[(i*16)+j]+16)%256])
-	key *= l//len(key)
-	key += key[:(l-len(key))]
-	return key
-
-def main():
-	file = open(input("Enter name of cache file to decrypt:"),"rb").read()
-	key = keygen(len(file))
-	filetowrite = open(input("Enter name of file to write: "),"wb")
-	for i in range(len(key)):
-		filetowrite.write(bytes([file[i]^key[i]]))	
+import argparse
 
 if __name__ == '__main__':
-	main()
+	parser = argparse.ArgumentParser(description='Decrypts VK android app cached mp3 music files.')
+	parser.add_argument('-i', '--infile', type=str, nargs='+', required=True)
+	parser.add_argument('-o', '--outfile', type=str, nargs='+')
+	args = parser.parse_args()
+
+	if args.outfile != None and len(args.outfile) != len(args.infile):
+		print('Error: number of outfiles should be equal to number of infiles')
+
+	if args.outfile == None:
+		args.outline = map(lambda x: x + '.mp3', args.infile)
+
+	for inf, outf in zip(args.infile, args.outfile):
+		try:
+			infile = open(inf, "rb")
+		except FileNotFoundError:
+			resp = input('Error: file {} not found, continue?(y/n)'.format(inf)).lower()
+			if resp == 'y': continue
+			exit()
+		outfile = open(outf, "wb")
+
+		filebyte = infile.read(1)
+		keybyte = 0x0D
+		while filebyte != b'':
+			outfile.write(bytes([filebyte[0] ^ keybyte]))
+			keybyte = (keybyte + 0x11) % 256
+			filebyte = infile.read(1)
+		
+		infile.close()
+		outfile.close()
+		print('Successfully decrypted:', outf)
